27dd7c9fc659045c6e84fda9222ad627e6702ac5
phuongvohuy <phuong.vo1908@gmail.com>
2021-02-08 15:23:25 +1300

    [UI] - Implement ui for home screen

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { AppointmentListService, Appointment } from '../appointment-list.service';
+import { AppointmentTableService, AppointmentColumn} from '../appointment-table.service';
+
+import {navigateToAppointmentDetailPage} from '../app-routing.module';
 
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
-  styleUrls: ['./home-screen.component.scss']
+	styleUrls: ['./home-screen.component.scss'],
 })
 export class HomeScreenComponent implements OnInit {
+	displayedColumns: Array<string> = [];
+	dataSource: Array<Appointment> = [];
+	columnsDataDefinition: Array<AppointmentColumn> = []
+	
+  constructor(
+		private appointmentTableService: AppointmentTableService,
+		private router: Router
+	) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  async ngOnInit(): Promise<void> {
+		this.dataSource = await this.appointmentTableService.retrieveDataSource();
+		this.displayedColumns = this.appointmentTableService.getDisplayColumns();
+		this.columnsDataDefinition = this.appointmentTableService.retrieveAppointmentColumnDefinition();
+	}
+	
+	onChooseRowHandler(appointmentItem: Appointment): void {
+		console.log('onChooseRowHandler', appointmentItem);
+		//this.router.navigate(['appointment-detail']);
+		navigateToAppointmentDetailPage(this.router, appointmentItem.id);
+	}
 }
