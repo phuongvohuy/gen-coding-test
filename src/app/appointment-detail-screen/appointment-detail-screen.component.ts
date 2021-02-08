2142ce89c551e1189a6d12fe36027708b6976888
phuongvohuy <phuong.vo1908@gmail.com>
2021-02-08 22:05:01 +1300

    [UI] - Implement layout for appointment_detail_screen

--- src/app/appointment-detail-screen/appointment-detail-screen.component.ts
+++ src/app/appointment-detail-screen/appointment-detail-screen.component.ts
@@ -1,4 +1,10 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap, Params } from '@angular/router';
+import { Observable } from 'rxjs';
+import { catchError, first, switchMap } from 'rxjs/operators';
+
+import {Appointment, AppointmentListService} from '../appointment-list.service';
 
 @Component({
   selector: 'app-appointment-detail-screen',
@@ -6,10 +12,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appointment-detail-screen.component.scss']
 })
 export class AppointmentDetailScreenComponent implements OnInit {
+	appointmentDetail: Appointment | undefined;
+	errorMessage: string | undefined;
+	isLoading: boolean = false;
 
-  constructor() { }
+  constructor(
+		private route: ActivatedRoute, 
+		private router: Router, 
+		private appointmentService: AppointmentListService
+	) { 
 
-  ngOnInit(): void {
+	}
+
+  async ngOnInit(): Promise<void> {
+		this.isLoading = true;
+		this.route.paramMap.pipe(
+			switchMap((param: ParamMap) => {
+				const appointmentId: number = Number(param.get('id'));
+				return this.appointmentService.getAppointmentDetailById(appointmentId)
+			})
+		).subscribe((data: Appointment) => {
+			this.appointmentDetail = data;
+			this.isLoading = false;
+		}, ((error: any) => {
+			this.errorMessage = error;
+			this.isLoading = false;
+		}))
   }
-
 }
