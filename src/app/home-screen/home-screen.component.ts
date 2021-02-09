599519f9260985c302ce367b4bffc0cd01c0f7f7
phuongvohuy <phuong.vo1908@gmail.com>
2021-02-09 18:56:11 +1300

    [UI] - Implement layout for home-screen with responsive

--- src/app/home-screen/home-screen.component.ts
+++ src/app/home-screen/home-screen.component.ts
@@ -1,34 +1,79 @@
 import { Component, OnInit } from '@angular/core';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Router } from '@angular/router';
 import { AppointmentListService, Appointment } from '../appointment-list.service';
 import { AppointmentTableService, AppointmentColumn} from '../appointment-table.service';
 
 import {navigateToAppointmentDetailPage} from '../app-routing.module';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Platform } from '@angular/cdk/platform';
+import { MatTableDataSource } from '@angular/material/table';
+import { AfterViewInit } from '@angular/core';
+
+const MAX_WIDTH = '(max-width: 600px)';
 
 @Component({
   selector: 'app-home-screen',
   templateUrl: './home-screen.component.html',
 	styleUrls: ['./home-screen.component.scss'],
 })
-export class HomeScreenComponent implements OnInit {
+export class HomeScreenComponent implements OnInit, AfterViewInit {
 	displayedColumns: Array<string> = [];
-	dataSource: Array<Appointment> = [];
-	columnsDataDefinition: Array<AppointmentColumn> = []
+	appointments: Array<any> = [];
+	dataSource: MatTableDataSource<Appointment>;
+	columnsDataDefinition: Array<AppointmentColumn> = [];
+	renderColumns: Array<AppointmentColumn> = [];
+
+	loading: boolean = true;
+	isMobile: boolean = false;
+	isSmallScreen: boolean = false;
 	
   constructor(
 		private appointmentTableService: AppointmentTableService,
-		private router: Router
-	) { }
+		private router: Router,
+		private breakpointObserver: BreakpointObserver,
+		private platform: Platform
+		
+	) {
+		const layoutChanges = breakpointObserver.observe(MAX_WIDTH);
+		layoutChanges.subscribe(result => {
+			this.isSmallScreen = breakpointObserver.isMatched(MAX_WIDTH);
+		});
+		
+		this.isMobile = platform.IOS || platform.ANDROID;
+
+		this.isSmallScreen = breakpointObserver.isMatched(MAX_WIDTH);
+		
+		this.dataSource = new MatTableDataSource(this.appointments);
+	 }
+
+	ngAfterViewInit() {
+		
+  }
 
   async ngOnInit(): Promise<void> {
-		this.dataSource = await this.appointmentTableService.retrieveDataSource();
+		this.loading = true;
+		this.appointments = await this.appointmentTableService.retrieveDataSource();
+		this.dataSource = new MatTableDataSource(this.appointments);
+		
 		this.displayedColumns = this.appointmentTableService.getDisplayColumns();
 		this.columnsDataDefinition = this.appointmentTableService.retrieveAppointmentColumnDefinition();
+		this.renderColumns = this.columnsDataDefinition.filter((item: AppointmentColumn) => {
+			return this.displayedColumns.find((displayItem: string) => displayItem === item.name) !== undefined;
+		})
+
+		this.loading = false;
 	}
 	
 	onChooseRowHandler(appointmentItem: Appointment): void {
-		console.log('onChooseRowHandler', appointmentItem);
-		//this.router.navigate(['appointment-detail']);
 		navigateToAppointmentDetailPage(this.router, appointmentItem.id);
+	}
+
+	onEditHandler(appointmentItem: Appointment): void {
+		navigateToAppointmentDetailPage(this.router, appointmentItem.id);
+	}
+
+	onDeleteHandler(appointmentItem: Appointment): void {
+		this.appointments = this.appointments.filter(item => item.id !== appointmentItem.id);
+		this.dataSource = new MatTableDataSource(this.appointments);
 	}
 }
